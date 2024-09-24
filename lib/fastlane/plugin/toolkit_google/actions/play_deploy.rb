3d0322852c807b46ca0cc70dcf7ec0370c4594af
Blair Replogle <UpBra@users.noreply.github.com>
2024-09-24 10:47:16 -0600

    update google play actions

--- lib/fastlane/plugin/toolkit_google/actions/play_deploy.rb
+++ lib/fastlane/plugin/toolkit_google/actions/play_deploy.rb
@@ -7,20 +7,10 @@ module Fastlane
 			PLAY_DEPLOY_APP_GOOGLE_PLAY_URL = :PLAY_DEPLOY_APP_GOOGLE_PLAY_URL
 		end
 
-		class PlayDeployAction < Action
+		class PlayDeployAction < SupplyAction
 
 			def self.run(params)
-				FastlaneCore::PrintTable.print_values(
-					config: params,
-					title: 'Summary for play_deploy',
-					mask_keys: [:json_key]
-				)
-
-				available = Fastlane::Actions::SupplyAction.available_options.map(&:key)
-				options = params.values.clone.keep_if { |k,v| available.include?(k) }
-				options.transform_keys(&:to_sym)
-
-				other_action.supply(options)
+				super(params)
 
 				name = ENV.fetch('SUPPLY_SETUP_NAME')
 				name ||= params[:package_name]
@@ -44,18 +34,6 @@ module Fastlane
 
 			def self.details
 				"Calls `supply` and constructs app display name and google play url"
-			end
-
-			def self.available_options
-				Fastlane::Actions::SupplyAction.available_options
-			end
-
-			def self.output
-				Fastlane::Actions::SupplyAction.output
-			end
-
-			def self.return_value
-				Fastlane::Actions::SupplyAction.return_value
 			end
 
 			def self.authors
